dbf8cdd4b57470025e68b87e7ebbb6319448ccb5
João Manfré <126287501+joaomanfre@users.noreply.github.com>
2023-06-16 16:55:58 -0300

    up

---
+++ js/cobra.js
@@ -0,0 +1,18 @@
+function joao() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://joaomanfre.github.io/js_jogo_cobrinha/' frameborder='0' height='750' width='1000'></iframe>"
+  }
+  function otavio() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://otaviozambon.github.io/js_jogo_cobrinha/' frameborder='0' height='750' width='1000'></iframe>"
+  }
+  function amanda() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://amandaolio.github.io/js_jogo_cobrinha/' frameborder='0' height='750' width='1000'></iframe>"
+  }
+  function yasmin() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://yasminferrazza.github.io/js_jogo_cobrinha/' frameborder='0' height='750' width='1000'></iframe>"
+  }
+  function gustavo() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://gustavdossant.github.io/snakegameg/' frameborder='0' height='750' width='1000'></iframe>"
+  }
+  function lucas() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://lucasvcarli.github.io/js_jogo_cobrinha/' frameborder='0' height='750' width='1000'></iframe>"
+  }
---
+++ js/velha.js
@@ -0,0 +1,18 @@
+function joao() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://joaomanfre.github.io/jogo-da-velha/' frameborder='0' height='680' width='1000'></iframe>"
+  }
+  function otavio() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://otaviozambon.github.io/jogo_da_velha/' frameborder='0' height='680' width='1000'></iframe>"
+  }
+  function amanda() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://amandaolio.github.io/jogo_da_velha/' frameborder='0' height='680' width='1000'></iframe>"
+  }
+  function yasmin() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://yasminferrazza.github.io/jogo_da_velha/' frameborder='0' height='680' width='1000'></iframe>"
+  }
+  function gustavo() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://gustavdossant.github.io/jgvelha/' frameborder='0' height='680' width='1000'></iframe>"
+  }
+  function lucas() {
+    document.getElementById("game-card").innerHTML = "<iframe src='https://lucasvcarli.github.io/jogo_da_velha/' frameborder='0' height='680' width='1000'></iframe>"
+  }
